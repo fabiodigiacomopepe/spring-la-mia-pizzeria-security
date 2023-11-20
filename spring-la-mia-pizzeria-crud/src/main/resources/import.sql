0cda5b6cbefbfb717c1ff8a400b7b68c0a03ce7e
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-11-20 17:35:36 +0100

    Basic request (Authentication): DONE

--- spring-la-mia-pizzeria-crud/src/main/resources/import.sql
+++ spring-la-mia-pizzeria-crud/src/main/resources/import.sql
@@ -11,3 +11,13 @@ INSERT INTO ingredients (name) VALUES('pomodoro');
 INSERT INTO ingredients (name) VALUES('mozzarella');
 INSERT INTO ingredients (name) VALUES('basilico');
 INSERT INTO ingredients (name) VALUES('prosciutto');
+
+INSERT INTO roles (id, name) VALUES(1, 'ADMIN');
+INSERT INTO roles (id, name) VALUES(2, 'USER');
+
+INSERT INTO users (first_name, last_name, email, password) VALUES('Mario', 'Rossi', 'mariorossi@gmail.com', '{noop}mario');
+INSERT INTO users (first_name, last_name, email, password) VALUES('Luca', 'Bianchi', 'lucabianchi@gmail.com', '{noop}luca');
+
+INSERT INTO users_roles (user_id, roles_id) VALUES(1, 1);
+INSERT INTO users_roles (user_id, roles_id) VALUES(1, 2);
+INSERT INTO users_roles (user_id, roles_id) VALUES(2, 2);
